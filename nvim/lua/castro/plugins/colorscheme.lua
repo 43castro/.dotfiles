d33729d71ea756bec41c8719f12c9a685dffa9eb
castro <xcaze8@gmail.com>
2025-03-25 16:45:25 +0100

    Upgraded colorscheme

--- nvim/lua/castro/plugins/colorscheme.lua
+++ nvim/lua/castro/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-  "kdheepak/monochrome.nvim",
+  "rose-pine/neovim",  
   priority = 1000,
   config = function()
-    vim.cmd("colorscheme monochrome")
+    vim.cmd("colorscheme rose-pine")
         -- Set background transparency
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
